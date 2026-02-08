88d762cc5cebaacc67e5bec6881d8d7885a5e6fa
xiena vij <vijxiena@gmail.com>
2026-02-08 23:31:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# NumPy Marks Analyzer 📊
+
+A beginner-friendly mini project using **Python + NumPy**.
+
+## Features
+- Calculates average, highest, and lowest marks
+- Finds topper using `argmax`
+- Assigns grades using NumPy logic
+- Normalizes marks between 0 and 1
+
+## Tech Used
+- Python
+- NumPy
+
+## How to Run
+
+```bash
+python marks_analyzer.py
+```
---
+++ marks_analyzer.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+#student marks dataset
+marks = np.array([78, 85, 92, 67, 88, 73, 95, 60])
+
+print("\n📌 Student Marks Analyzer")
+
+print("Marks: ", marks)
+
+#Basic Statistics
+avg = np.mean(marks)
+highest = np.max(marks)
+lowest = np.min(marks)
+std_dev = np.std(marks)
+
+print("\n📊 Statistics Summary:-\n")
+print(f"Average Marks      : {avg:.2f}")
+print(f"Highest Marks      : {highest}")
+print(f"Lowest Marks       : {lowest}")
+print(f"Standard Deviation : {std_dev:.2f}")
+
+#Topper position 
+topper_index = np.argmax(marks)
+print(f"\n🎓 Topper Studdent rollno : {topper_index+1}  Marks: {highest}")
+
+#grade distribution 
+grades = np.where(
+    marks >= 90, "A+",
+    np.where(marks >= 80, "A",
+    np.where(marks >= 70, "B",
+    np.where(marks >= 60, "C", "F")))
+)
+
+print("\n Grade Distribution:-")
+for i,grade in enumerate(grades):
+    print(f"Student roll no {i+1}  Marks : {marks[i]} -> Grade {grades}")
+
+#Normalization (0 to 1 scaling)
+normalized = (marks - lowest)/(highest - lowest) 
+print("\n Normalized Marks (0 to 1 scale): ")
+print(np.round(normalized, 2))
+
+print("\n ✅Analysis Complete!!!\n")
